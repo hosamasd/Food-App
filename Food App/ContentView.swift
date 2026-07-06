29e74e0bc4f7f28d4b69f8ec9d945abec8da0df2
hosam mohamed <34996617+hosamasd@users.noreply.github.com>
2026-07-06 05:07:33 +0300

    Initial Commit

---
+++ Food App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food App
+//
+//  Created by hosam on 06/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food App/Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AppApp.swift
+//  Food App
+//
+//  Created by hosam on 06/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
